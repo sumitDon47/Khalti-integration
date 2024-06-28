200c1ee3a1ee6fbf1f762be9810e8f437d48f185
Sumit <sumit@pop-os.localdomain>
2024-06-28 14:16:32 +0545

    update index.js file

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
-
+    f
 }
 connectToMongo()
 
@@ -58,7 +58,88 @@ app.post("/initialize-khali", async (req, res) => {
     }
 });
 
+// it is our `return url` where we verify the payment done by user
+app.get("/complete-khalti-payment", async (req, res) => {
+    const {
+        pidx,
+        txnId,
+        amount,
+        mobile,
+        purchase_order_id,
+        purchase_order_name,
+        transaction_id,
+    } = req.query;
+
+    try {
+        const paymentInfo = await verifyKhaltiPayment(pidx);
+
+        // Check if payment is completed and details match
+        if (
+            paymentInfo?.status !== "Completed" ||
+            paymentInfo.transaction_id !== transaction_id ||
+            Number(paymentInfo.total_amount) !== Number(amount)
+        ) {
+            return res.status(400).json({
+                success: false,
+                message: "Incomplete information",
+                paymentInfo,
+            });
+        }
+
+        // Check if payment done in valid item
+        const purchasedItemData = await PurchasedItem.find({
+            _id: purchase_order_id,
+            totalPrice: amount,
+        });
+
+        if (!purchasedItemData) {
+            return res.status(400).send({
+                success: false,
+                message: "Purchased data not found",
+            });
+        }
+        // updating purchase record 
+        await PurchasedItem.findByIdAndUpdate(
+            purchase_order_id,
+
+            {
+                $set: {
+                    status: "completed",
+                },
+            }
+        );
+
+        // Create a new payment record
+        const paymentData = await Payment.create({
+            pidx,
+            transactionId: transaction_id,
+            productId: purchase_order_id,
+            amount,
+            dataFromVerificationReq: paymentInfo,
+            apiQueryFromUser: req.query,
+            paymentGateway: "khalti",
+            status: "success",
+        });
+
+        // Send success response
+        res.json({
+            success: true,
+            message: "Payment Successful",
+            paymentData,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "An error occurred",
+            error,
+        });
+    }
+});
+
 app.listen(3001, () => {
     console.log("Backend listening at http://localhost:3001");
 });
+
+
 
